6466229d123883550ebce080bb7f19399ecf3309
Ricardo Herrera <rherrerapetit@gmail.com>
2015-01-03 20:26:18 -0500

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -11,7 +12,7 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-def test_home_page_returns_correct_html(self):
+    def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
